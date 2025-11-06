ff2d7dc0272d1c62b67aae9dd65e0fc24b006c79
ellacottg <ellacottg@gmail.com>
2025-11-06 10:31:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dishcovery/features/list/ListScreen.kt
@@ -0,0 +1,19 @@
+package com.example.dishcovery.features.list
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dishcovery.ui.theme.DishcoveryTheme
+
+@Composable
+fun ListScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ListScreenPreview() {
+    DishcoveryTheme {
+        ListScreen()
+    }
+}
---
+++ app/src/main/java/com/example/dishcovery/features/weeklyplan/WeeklyPlanScreen.kt
@@ -0,0 +1,19 @@
+package com.example.dishcovery.features.weeklyplan
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dishcovery.ui.theme.DishcoveryTheme
+
+@Composable
+fun WeeklyPlanScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WeeklyPlanScreenPreview() {
+    DishcoveryTheme {
+        WeeklyPlanScreen()
+    }
+}
