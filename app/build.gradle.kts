b2fe75b3f32c8eb7d9c9bbcfa18f3b7e89c4f4d1
ellacottg <ellacottg@gmail.com>
2025-11-06 10:41:23 -0500

    Implemented Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
+    implementation("androidx.navigation:navigation-compose:2.9.6")
 }
